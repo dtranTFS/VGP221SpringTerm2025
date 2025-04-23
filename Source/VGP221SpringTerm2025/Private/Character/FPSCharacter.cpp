8ad17ff6802278d45503d6699050b13b558cd72a
Danny Tran <dtran@torontofilmschool.ca>
2025-04-23 19:28:13 -0400

    Basic Projectile and Shooting
    Summary:
    - New projcetile class that gets assets from code
    - Updated player to shoot a projectile
    - Various assets added for examples

---
+++ Source/VGP221SpringTerm2025/Private/Character/FPSCharacter.cpp
@@ -0,0 +1,121 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (!FPSCameraComponent) {
+		FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+		FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+		FPSCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+		FPSCameraComponent->bUsePawnControlRotation = true;
+	}
+
+	if (!FPSMesh) {
+		FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+		FPSMesh->SetupAttachment(FPSCameraComponent);
+		FPSMesh->bCastDynamicShadow = false;
+		FPSMesh->CastShadow = false;
+	}
+
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+
+	// Look
+	PlayerInputComponent->BindAxis("LookHorizontal", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookVertical", this, &AFPSCharacter::AddControllerPitchInput);
+
+	// Jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPSCharacter::EndJump);
+
+	// Fire
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
+}
+
+void AFPSCharacter::MoveForward(float value)
+{
+	// 1. Unreal tutorial way
+	// FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+
+	FVector Direction = GetActorForwardVector();
+	AddMovementInput(Direction, value);
+}
+
+void AFPSCharacter::MoveRight(float value)
+{
+	FVector Direction = GetActorRightVector();
+	AddMovementInput(Direction, value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AFPSCharacter::EndJump()
+{
+	bPressedJump = false;
+}
+
+void AFPSCharacter::Fire()
+{
+	if (!ProjectileClass) return;
+
+	// Init relevant infomration for where the projectile will be
+	FVector CameraLocation;
+	FRotator CameraRotation;
+	GetActorEyesViewPoint(CameraLocation, CameraRotation);
+
+	MuzzleOffset.Set(100.0f, 0.0f, 0.0f);
+
+	FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
+
+	FRotator MuzzleRotation = CameraRotation;
+	MuzzleRotation.Pitch += 10.0f;
+
+	// Start of spawning the projectile
+	UWorld* World = GetWorld();
+	if (!World)  return;
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+	SpawnParams.Instigator = GetInstigator();
+
+	// Unity Instantiate
+	AFPSProjectile* Projectile = World->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+	if (!Projectile) return;
+
+	// Launch spawned projectile in the camera rotation
+	FVector LaunchDirection = MuzzleRotation.Vector();
+	Projectile->FireInDirection(LaunchDirection);
+}
+
--- Source/VGP221SpringTerm2025/Private/FPSProjectGameMode.cpp
+++ Source/VGP221SpringTerm2025/Private/FPSProjectGameMode.cpp
@@ -3,13 +3,25 @@
 
 #include "FPSProjectGameMode.h"
 
+
 void AFPSProjectGameMode::StartPlay()
 {
     Super::StartPlay();
 
     check(GEngine != nullptr);
 
-    // Display a debug message for five seconds. 
-    // The -1 "Key" value argument prevents the message from being updated or refreshed.
-    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameModeBase! Danny Test message"));
+    // 1. To print to screen
+    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameModeBase!"));
+
+    // 2. To print to console
+    // UE_LOG(LogTemp, Warning, TEXT("Hello World, this is FPSGameModeBase!"));
+
+    // 3. Printing values
+    // int TestNumber = 10;
+    // FString Name("TestName");
+    // bool BoolTest = true;
+    // UE_LOG(LogTemp, Warning, TEXT("TestNumber: %i, Name: %s, BoolTest: %d"), TestNumber, *Name, BoolTest);
+
+    // 4. Modern way of debugging values
+    // UE_LOGFMT(LogTemp, Warning, "TestNumber: {0}, Name: {1}, BoolTest: {2}", TestNumber, Name, BoolTest);
 }
